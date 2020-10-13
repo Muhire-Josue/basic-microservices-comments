2e166e173ac26a649423f72c7b2dcae7327d2b86
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-13 12:40:42 +0200

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 const commentsByPostId = {};
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/posts/:id/comments', (req, res) => {
 
