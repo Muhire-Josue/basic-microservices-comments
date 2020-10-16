69c04fcbedde5bc45402ea4005f60f0d859f03ab
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-10-16 10:32:55 +0200

    add comment moderation

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Event Received:', req.body.type);
 
     const { type, data } = req.body;
 
     if (type === 'CommentModerated') {
-        const { postId, id, status } = data;
+        const { postId, id, status, content } = data;
 
         const comments = commentsByPostId[postId];
 
@@ -53,6 +53,17 @@ app.post('/events', (req, res) => {
         });
 
         comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                content,
+                postId,
+                status
+            }
+        });
+
     }
 
     res.send({});
